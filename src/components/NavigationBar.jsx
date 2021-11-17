943af36590bde57d31e69cd48ebbc1ebea3cdc3c
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2021-11-17 00:25:57 -0700

    Further Styling

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "./NavigationBar.module.css";
 
 class NavigationBar extends Component {
   render() {
     return (
-      <Navbar className="navbar navbar-dark bg-secondary">
-        <h1 className={styles.brandName}>
-          Our Trips
-        </h1>
+      <Nav className="navbar navbar-dark bg-secondary">
+        <div className="container-fluid">
+          <h1 className={styles.brandName}>
+            Our Trips
+          </h1>
 
-        <Link to ="/" className={styles.navLinks}> 
-          My Account
-        </Link>
-      </Navbar>
+          <div>
+          <Link to="/" className={styles.navLinks}>
+            Home
+          </Link>
+          <Link to="/" className={styles.navLinks}>
+            My Account
+          </Link>
+          </div>
+         
+        </div>
+
+      </Nav>
     );
   }
 }

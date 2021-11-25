4465b18e040d6553ce978420288e733d6a0dfd06
Etienne <etienne.lagace@shaw.ca>
2021-11-25 10:30:50 -0700

    changes navigation bar to header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import AccountPage from "./components/AccountPage";
 import HomePage from "./components/HomePage";
-import NavigationBar from "./components/NavigationBar";
+import Header from "./components/Header";
 import PrivateTripPage from "./components/PrivateTripPage";
 import PublicTripPage from "./components/PublicTripPage";
 
 const App = () => {
   return (
     <React.Fragment>
-      <NavigationBar />
+      <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />

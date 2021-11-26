3e3afbe0693bf17a19a220bad9ded0ef5281eeb6
Etienne <etienne.lagace@shaw.ca>
2021-11-25 21:59:49 -0700

    added event list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import AccountPage from "./components/account/AccountPage";
 import InvitePage from "./components/account/invite/InvitePage";
+import Events from "./components/events/EventPage";
+import EventList from "./components/events/EventList";
 import HomePage from "./components/HomePage";
 import PrivateTripPage from "./components/PrivateTripPage";
 import PublicTripPage from "./components/PublicTripPage";
@@ -15,6 +17,8 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/account/*" element={<AccountPage />} />
           <Route path="/account/invite" element={<InvitePage />} />
+          <Route path="/account/events" element={<Events />} />
+          <Route path="/account/eventlist" element={<EventList />} />
           <Route path="/publictrips/*" element={<PublicTripPage />} />
           <Route path="/privatetrips/*" element={<PrivateTripPage />} />
         </Routes>

fb27928735cc53da8f08d4039ddce43d9c7f5147
Etienne <etienne.lagace@shaw.ca>
2021-11-25 21:59:49 -0700

    added event list

---
+++ src/components/events/EventPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../account/HeaderAccount";
+import styles from "./EventPage.module.css";
+
+const EventPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default EventPage;

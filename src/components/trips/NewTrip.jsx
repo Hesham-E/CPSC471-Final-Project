9404cdbd8c12731033a3399cd5823982951d20bb
Etienne <etienne.lagace@shaw.ca>
2021-11-25 22:59:10 -0700

    added new trip page

--- src/components/trips/NewTrip.jsx
+++ src/components/trips/NewTrip.jsx
@@ -1,9 +1,44 @@
-import styles from "./NewTrip.css";
+import Header from "../account/HeaderAccount";
+import Button from "../parts/Button";
+import "./NewTrip.css";
 
 const NewTrip = () => {
   return (
     <div>
-      <div></div>
+      <Header />
+      <div className="user-sign-up">
+        <div className="background-rectangle" />
+        <div className="question-bar">
+          <p className="question-title">Name</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Description</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Date</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Time</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Location</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Send Invitation</p>
+          <div className="form-field" />
+        </div>
+        <div className="question-bar">
+          <p className="question-title">Private or Public?</p>
+          <div className="form-field-trueorfalse" />
+        </div>
+
+        <Button className="button-button" />
+      </div>
     </div>
   );
 };

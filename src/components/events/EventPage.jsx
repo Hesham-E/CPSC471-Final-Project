97cdd106f6df75d5887346cfb2588e3fe6fda963
Etienne <etienne.lagace@shaw.ca>
2021-11-25 22:22:27 -0700

    added event page

--- src/components/events/EventPage.jsx
+++ src/components/events/EventPage.jsx
@@ -1,10 +1,19 @@
 import Header from "../account/HeaderAccount";
+import Event from "./EventDesc";
+import Button from "../parts/Button";
 import styles from "./EventPage.module.css";
 
 const EventPage = () => {
   return (
     <div>
       <Header />
+      <div className={styles.mainDiv}>
+        <div className={styles.alignDiv}>
+          <span className={styles.text1}>Event</span>
+          <Button />
+        </div>
+        <Event />
+      </div>
     </div>
   );
 };

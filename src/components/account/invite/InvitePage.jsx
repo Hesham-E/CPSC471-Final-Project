3980cf901adbd08948d4104306140d873740eacf
Etienne <etienne.lagace@shaw.ca>
2021-12-16 21:37:25 -0700

    middle of rebase 2

---
+++ src/components/account/invite/InvitePage.jsx
@@ -0,0 +1,14 @@
+import Header from "../HeaderAccount";
+import Body from "./InviteBody";
+import styles from "./InvitePage.module.css";
+
+const InvitePage = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default InvitePage;
